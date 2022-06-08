a6ab32712f5e1e47f0dbbd659620915c800b8396
tomi <tomiwolfman@gmail.com>
2022-06-08 12:40:30 -0300

    Added chakra ui and moralis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ChakraProvider>
+      <MoralisProvider appId={process.env.NEXT_PUBLIC_APPid} serverUrl={process.env.NEXT_PUBLIC_Server_URL}>
+        <Component {...pageProps}/>
+      </MoralisProvider>
+
+    </ChakraProvider>
+
+  )
+  
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-     <h1>Hello world</h1>
+    <div >
+     <h1>Hello     world</h1>
+     <h2>jajajja</h2>
     </div>
   )
 }
