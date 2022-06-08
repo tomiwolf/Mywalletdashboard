c8ec2ba2d27f2f0392ec8c74ba1dd52eaa7c475b
tomi <tomiwolfman@gmail.com>
2022-06-08 20:47:39 -0300

    fix deploy

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,9 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }) {
   return(
     <ChakraProvider>
-      <MoralisProvider appId={process.env.NEXT_PUBLIC_APPid} serverUrl={process.env.NEXT_PUBLIC_Server_URL}>
+      {/* <MoralisProvider appId={process.env.NEXT_PUBLIC_APPid} serverUrl={process.env.NEXT_PUBLIC_Server_URL}> */}
+      <MoralisProvider appId={IYWSbIXRzNtjErC92zo2mXVJLq18c3oiQ8w6igV4} serverUrl={"https://usb79g3kxs9t.usemoralis.com:2053/server"}>
+
         <Component {...pageProps}/>
       </MoralisProvider>
 
